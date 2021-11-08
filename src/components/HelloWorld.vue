52f6f450123fe4d76f3740057c709c9c3fb7596c
Kolyan525 <Kolyakalina2001@gmail.com>
2021-11-08 17:42:54 +0200

    Added IOB.svg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
-          <br>please join our online
+          <br>pease join our online
           <a
             href="https://community.vuetifyjs.com"
             target="_blank"
